8e6e3182b6cd43766d6d5668aa6d6110e0bbed94
Nikita Gupta <nikitagupta9571@gmail.com>
2020-01-30 09:44:22 +0530

    Assignment 3

--- Assignments/AngularAssignments/ass2/src/app/app-routing.module.ts
+++ Assignments/AngularAssignments/ass2/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { FormvalidationComponent } from './formvalidation/formvalidation.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Login', component: FormvalidationComponent},
+  {path: 'Register', component: RegisterFormComponent},
+  {path: 'Update', component: EditUserComponent},
+  {path: 'Show', component: ListUserComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Assignments/AngularAssignments/ass2/src/app/showemp/showemp.component.ts
+++ Assignments/AngularAssignments/ass2/src/app/showemp/showemp.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MatTableModule } from '@angular/material';
+export interface Emp {
+  id: string;
+  name: string;
+  salary: string;
+  department: string;
+}
+
+
 
 @Component({
   selector: 'app-showemp',
@@ -6,10 +15,12 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./showemp.component.css']
 })
 export class ShowempComponent implements OnInit {
-@Input() details = [];
+@Input() details = [{id: '101', name: 'abc', salary: '21321', department: 'abc'},
+{id: '101', name: 'abc', salary: '21321', department: 'abc'}];
+ELEMENT_DATA: Emp[] = this.details;
+displayedColumns: string [] = ['id', 'name', 'salary', 'department'];
+dataSource = this.ELEMENT_DATA;
   constructor() { }
-
   ngOnInit() {
   }
-
 }
