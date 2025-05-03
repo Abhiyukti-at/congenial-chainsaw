ab63b156d718550f3ff57491acac09b4fca5b3fe
Nikita Gupta <nikitagupta9571@gmail.com>
2025-05-03 17:44:51 +0530

    preFilter and postFilter for method security

--- Bank App/src/main/java/io/nikita/BankApp/Controller/ContactController.java
+++ Bank App/src/main/java/io/nikita/BankApp/Controller/ContactController.java
@@ -3,11 +3,14 @@ package io.nikita.BankApp.Controller;
 import io.nikita.BankApp.Models.ContactMessages;
 import io.nikita.BankApp.repository.ContactMessagesRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -16,10 +19,18 @@ public class ContactController {
     private final ContactMessagesRepository contactMessagesRepository;
 
     @PostMapping("/contact")
-    public ContactMessages getContactDetails(@RequestBody ContactMessages contactMessages) {
-        contactMessages.setContactId(getServiceReqNumber());
-        contactMessages.setCreateDt(new Date(System.currentTimeMillis()));
-        return contactMessagesRepository.save(contactMessages);
+//    @PreFilter("filterObject.contactName != 'Test'")
+    @PreFilter("filterObject.contactName != 'Test'")
+    public List<ContactMessages> getContactDetails(@RequestBody List<ContactMessages> contactMessages) {
+        List<ContactMessages> contacts = new ArrayList<>();
+        if (!contactMessages.isEmpty()) {
+            ContactMessages contact = contactMessages.getFirst();
+            contact.setContactId(getServiceReqNumber());
+            contact.setCreateDt(new Date(System.currentTimeMillis()));
+            ContactMessages savedContact = contactMessagesRepository.save(contact);
+            contacts.add(savedContact);
+        }
+        return contacts;
     }
 
     public String getServiceReqNumber() {
