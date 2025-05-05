9836c2c85125895d720810cc9692d3bb82fc54e9
Nikita Gupta <nikitagupta9571@gmail.com>
2025-05-05 09:36:34 +0530

    Added auth server changes for Opaque token

--- Bank App/src/main/java/io/nikita/BankApp/Configuration/KeyclockOpaqueRoleConverter.java
+++ Bank App/src/main/java/io/nikita/BankApp/Configuration/KeyclockOpaqueRoleConverter.java
@@ -7,30 +7,41 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenAuthenticationConverter;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class KeyclockOpaqueRoleConverter implements OpaqueTokenAuthenticationConverter {
     /**
-     *
-     * @param introspectedToken the bearer token used to perform token introspection
+     * @param introspectedToken      the bearer token used to perform token introspection
      * @param authenticatedPrincipal the result of token introspection
      * @return
      */
+//    @Override
+//    public Authentication convert(String introspectedToken, OAuth2AuthenticatedPrincipal authenticatedPrincipal) {
+//        String username = authenticatedPrincipal.getAttribute("preferred_username");
+//        Map<String,Object> realmAccess = (Map<String, Object>) authenticatedPrincipal.getAttribute("realm_access");
+//        if(realmAccess != null && !realmAccess.isEmpty()) {
+//            Collection<GrantedAuthority> roles = ((List<String>)realmAccess.get("roles"))
+//                    .stream()
+//                    .map(role -> "ROLE_" + role)
+//                    .map(SimpleGrantedAuthority::new)
+//                    .collect(Collectors.toList());
+//            return new UsernamePasswordAuthenticationToken(authenticatedPrincipal.getName(), null, roles);
+//        }
+//        return null;
+//    }
     @Override
     public Authentication convert(String introspectedToken, OAuth2AuthenticatedPrincipal authenticatedPrincipal) {
-        String username = authenticatedPrincipal.getAttribute("preferred_username");
-        Map<String,Object> realmAccess = (Map<String, Object>) authenticatedPrincipal.getAttribute("realm_access");
-        if(realmAccess != null && !realmAccess.isEmpty()) {
-            Collection<GrantedAuthority> roles = ((List<String>)realmAccess.get("roles"))
-                    .stream()
-                    .map(role -> "ROLE_" + role)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
-            return new UsernamePasswordAuthenticationToken(authenticatedPrincipal.getName(), null, roles);
-        }
-        return null;
+
+        ArrayList<String> roles = authenticatedPrincipal.getAttribute("scope");
+
+        Collection<GrantedAuthority> grantedAuthorities = roles
+                .stream()
+                .map(role -> "ROLE_" + role)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return new UsernamePasswordAuthenticationToken(authenticatedPrincipal.getName(), null, grantedAuthorities);
+
     }
 }
