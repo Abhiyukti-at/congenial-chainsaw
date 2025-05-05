ee1fa69c8133f27aa81aaf2c94843d6998ee1710
Nikita Gupta <nikitagupta9571@gmail.com>
2025-05-05 14:29:45 +0530

    Added new jpa project

---
+++ JPA-Utitlity/src/main/java/io/nikita/JPAUtitlity/JpaUtitlityApplication.java
@@ -0,0 +1,13 @@
+package io.nikita.JPAUtitlity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaUtitlityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaUtitlityApplication.class, args);
+	}
+
+}
---
+++ JPA-Utitlity/src/test/java/io/nikita/JPAUtitlity/JpaUtitlityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nikita.JPAUtitlity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaUtitlityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
