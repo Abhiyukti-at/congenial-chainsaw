187a95be49764bfd3e6506ffea96bc166b8bedf0
Nikita Gupta <nikitagupta9571@gmail.com>
2025-05-05 09:37:12 +0530

    Added auth server changes for Opaque token

---
+++ OAuth2AuthServer/src/main/java/io/nikita/OAuth2AuthServer/model/Authority.java
@@ -0,0 +1,21 @@
+package io.nikita.OAuth2AuthServer.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "authorities")
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+}
